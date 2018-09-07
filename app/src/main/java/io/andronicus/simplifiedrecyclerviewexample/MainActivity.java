702ed0e3f571db9b3b69e9b0c2a1734dad3aface
andronicus-kim <androkim.kim7@gmail.com>
2018-09-07 11:07:50 +0300

    adds module dependency to example app

--- app/src/main/java/io/andronicus/simplifiedrecyclerviewexample/MainActivity.java
+++ app/src/main/java/io/andronicus/simplifiedrecyclerviewexample/MainActivity.java
@@ -3,11 +3,17 @@ package io.andronicus.simplifiedrecyclerviewexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import io.andronicus.simplifiedrecyclerview.Adapter;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
