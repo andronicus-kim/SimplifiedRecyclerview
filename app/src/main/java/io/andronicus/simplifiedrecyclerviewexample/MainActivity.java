9a7254125251a521081f5ec32f3feb60549ec6d7
andronicus-kim <androkim.kim7@gmail.com>
2018-09-07 11:37:03 +0300

    implements binding data to views

--- app/src/main/java/io/andronicus/simplifiedrecyclerviewexample/MainActivity.java
+++ app/src/main/java/io/andronicus/simplifiedrecyclerviewexample/MainActivity.java
@@ -2,6 +2,9 @@ package io.andronicus.simplifiedrecyclerviewexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,7 +23,12 @@ public class MainActivity extends AppCompatActivity implements MyAdapter.ViewHol
     }
 
     @Override
-    public void onViewHolderClick(String item) {
+    public void bindDataToViews(String item, View view) {
+        TextView textView = view.findViewById(R.id.tv_test);
+        textView.setText(item);
+    }
 
+    @Override
+    public void onViewHolderClick(String item) {
     }
 }
